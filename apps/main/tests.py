602db2e162bdd96b64a7331910bc07e2af09de9a
Sajan4072 <rai4072@gmail.com>
2022-07-21 18:07:56 +0545

    tests(testcase):add post_related_tests

--- apps/main/tests.py
+++ apps/main/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+import json
+
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+class PostTestCase(APITestCase):
+    def test_post_request(self):
+        url = reverse('calculate_fare')
+        data = {
+            "time": "12:00",
+            "distance": "2"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'fare': '100.0'})
